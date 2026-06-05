2eb466911ac4e19b85f068d94af833f729035879
Steven Andre <stevenwu04@gmail.com>
2026-06-05 15:09:18 +0700

    database initial setup

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"taskflow/internal/models"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
+
+	dsn := "host=localhost user=stevengonassis dbname=taskflow port=5432 sslmode=disable"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Database connection failed")
+	}
+
+	db.AutoMigrate(&models.User{})
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
